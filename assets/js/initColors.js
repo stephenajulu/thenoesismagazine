edf3f513650f59392dd9ec4139035251b5713473
stephenajulu <alunje73@gmail.com>
2021-03-05 18:29:33 +0300

    added meta & icons

--- assets/js/initColors.js
+++ assets/js/initColors.js
@@ -1,25 +1,5 @@
 
 let be = document.getElementsByTagName("BODY")[0];
-let styleVariables = [
-    'primary',
-    'secondary',
-    'grey',
-    'background-color',
-    'accent',
-    'hover',
-    'gradient',
-    'articleText',
-    'track',
-    'progress',
-    'card',
-    'error',
-    'success',
-    'errorBackground',
-    'horizontalRule',
-    'inputBackground',
-    'tooltip',
-];
-
 
 initColors();
 
@@ -38,11 +18,41 @@ function initColors(){
 
 
 function makeDark(){
-styleVariables.forEach(varName => be.style.setProperty('--' + varName, 'var(--dark-' + varName + ')'));
-
+    be.style.setProperty('--primary', "#fff");
+    be.style.setProperty('--secondary', "#fff");
+    be.style.setProperty('--grey', "#73737D");
+    be.style.setProperty('--background-color', "#111216");
+    be.style.setProperty('--accent', "#CD2653");
+    be.style.setProperty('--hover', "rgba(255, 255, 255, 0.07)");
+    be.style.setProperty('--gradient', "linear-gradient(180deg, #111216 0%, rgba(66, 81, 98, 0.36) 100%)");
+    be.style.setProperty('--articleText', "#fff");
+    be.style.setProperty('--track', "rgba(255, 255, 255, 0.3)");
+    be.style.setProperty('--progress', "#fff");
+    be.style.setProperty('--card', "#1D2128");
+    be.style.setProperty('--error', "#EE565B");
+    be.style.setProperty('--success', "#46B17B");
+    be.style.setProperty('--errorBackground', "rgba(238, 86, 91, 0.1)");
+    be.style.setProperty('--horizontalRule', "rgba(255, 255, 255, 0.15)");
+    be.style.setProperty('--inputBackground', "rgba(255, 255, 255, 0.07)");
+    be.style.setProperty('--tooltip', "#000");
 }
 
 function makeLight(){
-styleVariables.forEach(varName => be.style.setProperty('--' + varName, ''));
-
+    be.style.setProperty('--primary', "");
+    be.style.setProperty('--secondary', "");
+    be.style.setProperty('--grey', "");
+    be.style.setProperty('--background-color', "");
+    be.style.setProperty('--accent', "");
+    be.style.setProperty('--hover', "");
+    be.style.setProperty('--gradient', "");
+    be.style.setProperty('--articleText', "");
+    be.style.setProperty('--track', "");
+    be.style.setProperty('--progress', "");
+    be.style.setProperty('--card', "");
+    be.style.setProperty('--error', "");
+    be.style.setProperty('--success', "");
+    be.style.setProperty('--errorBackground', "");
+    be.style.setProperty('--horizontalRule', "");
+    be.style.setProperty('--inputBackground', "");
+    be.style.setProperty('--tooltip', "lightgrey");
 }
